3e813fec550ddda8727d7a5645a9448634e8d860
Stefan Jacobs <stefan.jacobs@absa.africa>
2021-05-06 21:40:13 +0200

    Initial push and scafholding

---
+++ TaxCalculator.Domain/Entities/TaxCalculated.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxCalculator.Domain.Entities
+{
+    public class TaxCalculated
+    {
+
+        public TaxCalculated(string postalCode, long income)
+        {
+            PostalCode = postalCode;
+            Income = income;
+        }
+
+        public Guid Id { get; private set; }
+        public decimal CalculatedAmount { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+
+        public string PostalCode { get; private set; }
+        public long Income { get; private set; }
+
+        public void CalculateTaxTotla(string postalCode, long income)
+        {
+            this.CalculatedAmount = 100;
+        }
+    }
+}
